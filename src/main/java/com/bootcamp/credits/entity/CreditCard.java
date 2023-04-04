b1d17848b1a0d54ad23e31fc5fd71ae7b884a94c
Alan Rufino Chumpitaz Yaya <achumpiy@emeal.nttdata.com>
2023-04-03 23:30:54 -0500

    credits

--- src/main/java/com/bootcamp/credits/entity/CreditCard.java
+++ src/main/java/com/bootcamp/credits/entity/CreditCard.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
